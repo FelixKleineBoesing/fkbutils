ea599a2f5de5b030361215cf5dfc8b1f3b4c33c6
Felix <felix.boesing@t-online.de>
2021-02-08 14:16:36 +0100

    add config manager for

---
+++ fkbutils/misc/config_manager.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from dotenv.main import load_dotenv
+
+
+class ConfigManager:
+    """
+    The config manager accesses environment variables and default variables and can therefore be used in
+    local development mode as well as in dockerized production
+    """
+
+    def __init__(self, default_variables: dict = None, env_file: str = None):
+        """
+
+        :param env_file:
+        """
+        if default_variables is None:
+            default_variables = {}
+        self.default_variables = default_variables
+        if env_file is not None:
+            if os.path.isfile(env_file):
+                load_dotenv(env_file)
+            else:
+                logging.error("env file is not found")
+
+    def get_value(self, key: str):
+        if key in self.default_variables:
+            return self.default_variables[key]
+        elif key in os.environ:
+            return os.environ[key]
+        else:
+            raise KeyError("Key not present!")
+
+    def overwride_value(self, key: str, value):
+        self.default_variables[key] = value
--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
+from setuptools import find_packages
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='fkbutils',
-      version="0.1.1",
+      version="0.2.0",
       description='Utility functions that I need often across multiple projects!',
       install_requires=required,
       include_package_data=True,
       zip_safe=False,
       author="Felix Kleine Bösing",
       author_email="felix.boesing@t-online.de",
-      packages=["fkbutils"])
+      packages=find_packages())
